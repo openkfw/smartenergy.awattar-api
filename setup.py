36c80e8dd5f59cc09394808b2e81f83507fc67f4
melissa.schmidt <melissa.schmidt@accenture.com>
2023-01-20 08:50:17 +0100

    Add parameters to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,28 @@
 """Package setup"""
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="electricity_maps_api",
+    version="0.2.0",
+    author="",
+    author_email="",
+    description="A Python API for receiving energy prices from the Awattar API",
+    license="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    provides=["electricity_maps_api"],
+    install_requires=["requests"],
+    setup_requires=["wheel"],
+)
